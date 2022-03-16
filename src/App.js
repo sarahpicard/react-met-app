d48fce79cb999d1ce186e5b83ae94b1fd455ad4f
sarahpicard <sarahannrey225@gmail.com>
2022-03-16 12:56:46 -0400

    stub nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div>
+      <Nav />
       <h1>REACT MET Museum App</h1>
     </div>
   );
---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+
+const Nav = () => {
+  return (
+    <div className="nav-bar">
+      <a className="nav-item" href="/all-art">All Art</a>
+      <a className="nav-item" href="/all-artists">All Artists</a>
+    </div>
+  )
+}
+
+export default Nav
