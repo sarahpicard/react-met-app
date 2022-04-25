1d052bce04d3563603b069ee817fa1071a3b4714
sarahpicard <sarahannrey225@gmail.com>
2022-04-25 12:08:31 -0400

    add link to art container

--- src/pages/ArtList/ArtList.jsx
+++ src/pages/ArtList/ArtList.jsx
@@ -1,8 +1,5 @@
 import { useEffect, useState } from "react"
-import { getArtList, getOne } from "../../services/api-calls"
-import { Link } from "react-router-dom"
-import { random } from "animejs"
-
+import { getOne } from "../../services/api-calls"
 const ArtList = () => {
   const [oneArt, setOneArt] = useState({})
 
@@ -14,29 +11,28 @@ const ArtList = () => {
 
   return (
     <div className="w-screen">
-      <div className="container mx-auto w-1/3 flexbox my-20 bg-rose-50 px-6 py-6 rounded-lg shadow-lg">
-        <h1 className="text-2xl font-medium mb-2">{oneArt.title}</h1>
-        <p>
-          <span className="font-medium">Artist: </span> 
-          {oneArt.artistDisplayName}
-        </p>
-        <p>
-          <span className="font-medium">Art Date: </span> 
-          {oneArt.objectDate}
-        </p>
-        <p>
-          <span className="font-medium">Art Culture: </span>
-          {oneArt.culture}
-        </p>
-        <p>
-          <span className="font-medium">Art repository: </span>
-          {oneArt.repository}
-        </p>
-        <img className="mx-auto mb-3 mt-7" src={oneArt.primaryImage} alt="art" style={{height: '300px', width: '300px'}}/>
-        {/* <button>
-          <a href={oneArt.objectURL}>See Art</a>
-        </button> */}
-      </div>
+      <a href={oneArt.objectURL} target="_blank" rel="noreferrer noopener">
+        <div className="container mx-auto w-1/3 flexbox my-20 bg-rose-50 px-6 py-6 rounded-lg shadow-lg">
+          <h1 className="text-2xl font-medium mb-2">{oneArt.title}</h1>
+          <p>
+            <span className="font-medium">Artist: </span> 
+            {oneArt.artistDisplayName}
+          </p>
+          <p>
+            <span className="font-medium">Art Date: </span> 
+            {oneArt.objectDate}
+          </p>
+          <p>
+            <span className="font-medium">Art Culture: </span>
+            {oneArt.culture}
+          </p>
+          <p>
+            <span className="font-medium">Art repository: </span>
+            {oneArt.repository}
+          </p>
+          <img className="mx-auto mb-3 mt-7" src={oneArt.primaryImage} alt="art" style={{height: '320px', width: '300px'}}/>
+        </div>
+      </a>
     </div>
   )
 }
