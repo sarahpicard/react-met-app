4045d2895c11190287c4c1eeac2d7db09377c054
sarahpicard <sarahannrey225@gmail.com>
2022-04-25 11:35:50 -0400

    implement tailwindcss and adjust nav styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,10 @@
-
 const Nav = () => {
   return (
-    <div className="nav-bar">
-      <a className="nav-item" href="/art">Art</a>
-      <a className="nav-item" href="/artists">Artists</a>
+    <div className="text-3xl w-screen bg-red-900 h-20">
+      <div className="pt-5 text-white">
+        <a className="px-10  hover:text-red-200" href="/art">Art</a>
+        <a className="hover:text-red-200" href="/artists">Artists</a>
+      </div>
     </div>
   )
 }
