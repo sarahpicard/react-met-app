9294d96409382cde697fa2a530537fe0559aa4d1
sarahpicard <sarahannrey225@gmail.com>
2022-04-25 11:57:40 -0400

    adjust basic styling for random art card

--- src/pages/ArtList/ArtList.jsx
+++ src/pages/ArtList/ArtList.jsx
@@ -15,15 +15,27 @@ const ArtList = () => {
   return (
     <div className="w-screen">
       <div className="container mx-auto w-1/3 flexbox my-20 bg-rose-50 px-6 py-6 rounded-lg shadow-lg">
-        <h1>{oneArt.title}</h1>
-        <p>Artist: {oneArt.artistDisplayName}</p>
-        <p>Art Date: {oneArt.objectDate}</p>
-        <p>Art Culture: {oneArt.culture}</p>
-        <p>Art repository: {oneArt.repository}</p>
-        <img src={oneArt.primaryImage} alt="art" style={{height: '300px', width: '300px'}}/>
-        <button>
+        <h1 className="text-2xl font-medium mb-2">{oneArt.title}</h1>
+        <p>
+          <span className="font-medium">Artist: </span> 
+          {oneArt.artistDisplayName}
+        </p>
+        <p>
+          <span className="font-medium">Art Date: </span> 
+          {oneArt.objectDate}
+        </p>
+        <p>
+          <span className="font-medium">Art Culture: </span>
+          {oneArt.culture}
+        </p>
+        <p>
+          <span className="font-medium">Art repository: </span>
+          {oneArt.repository}
+        </p>
+        <img className="mx-auto mb-3 mt-7" src={oneArt.primaryImage} alt="art" style={{height: '300px', width: '300px'}}/>
+        {/* <button>
           <a href={oneArt.objectURL}>See Art</a>
-        </button>
+        </button> */}
       </div>
     </div>
   )
